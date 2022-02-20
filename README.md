a3ffd82948d6484b11e28026b8c6113b5de23104
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-02-20 11:37:06 +0100

    Update timer style and reset logic

--- README.md
+++ README.md
@@ -1,13 +1,39 @@
 # Timer
 
-Simple countdown timer widget.
+<div align="center">
+  <br />
+  <a href="https//dstrekelj.github.io/algebra-example-timer" target="_blank">
+    <img src="image.png" alt="Timer widget screenshot" width="500">
+    <br />
+    <br />
+    <b>See it in action &mdash; https://dstrekelj.github.io/algebra-example-timer</b>
+  </a>
+  <br />
+  <br />
+</div>
+
+---
+
+## Features
+
+### Current
+
+- [x] Configure element to serve as timer with `timer` ID
+- [x] Configure starting time with `data-start-time` attribute
+- [x] Configure time when blinking begins with `data-blink-time` attribute
+- [x] Configure element to serve as timer start action with `data-action-start` attribute
+
+### Upcoming
+
+- [ ] Support for creating multiple timer instances
+- [ ] SCSS styling
 
 ## Instructions
 
 Include the stylesheet in the head of your document:
 
 ```html
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style.css" />
 ```
 
 Include the script at the end of the body of your document:
--- app.js
+++ app.js
@@ -1,27 +1,41 @@
-var timerElement = document.getElementById('timer');
-var startButton = document.querySelector('[data-action-start]');
+const timerElement = document.getElementById("timer");
+const startButton = document.querySelector("[data-action-start]");
+const timerElementStyle = getComputedStyle(timerElement);
+const initialFontSize = parseInt(timerElementStyle.fontSize);
 
-var timerElementStyle = getComputedStyle(timerElement);
-var initialFontSize = parseInt(timerElementStyle.fontSize);
+let intervalId = null;
 
-var intervalId = null;
+function reset() {
+  timerElement.innerHTML = parseInt(timerElement.dataset.startTime);
+  timerElement.style.fontSize = initialFontSize + "px";
+  timerElement.classList.toggle("blink", false);
+  timerElement.classList.toggle("expired", false);
+}
 
-startButton.addEventListener('click', function () {
-  var time = parseInt(timerElement.dataset.startTime);
-  var blinkTime = parseInt(timerElement.dataset.blinkTime);
-  var fontSize = initialFontSize;
+function update(time, blinkTime, fontSize) {
+  timerElement.innerHTML = time;
+  timerElement.style.fontSize = fontSize + "px";
+  timerElement.classList.toggle("blink", time > 0 && time <= blinkTime);
+  timerElement.classList.toggle("expired", time === 0);
+}
+
+reset();
+
+startButton.addEventListener("click", function () {
+  reset();
+
+  const blinkTime = parseInt(timerElement.dataset.blinkTime);
+
+  let time = parseInt(timerElement.dataset.startTime) - 1;
+  let fontSize = initialFontSize;
 
   if (intervalId !== null) {
     clearInterval(intervalId);
   }
 
   intervalId = setInterval(function () {
-    timerElement.innerHTML = time;
-    timerElement.style.fontSize = fontSize + 'px';
-    fontSize += 5;
-
-    timerElement.classList.toggle('blink', time > 0 && time < blinkTime);
-    timerElement.classList.toggle('expired', time === 0);
+    update(time, blinkTime, fontSize);
+    fontSize += 16;
 
     if (time === 0) {
       clearInterval(intervalId);
